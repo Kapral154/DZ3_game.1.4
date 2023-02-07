85179c271f0c3b4974633653f81937f3a402f7b2
Kapral154 <113196524+Kapral154@users.noreply.github.com>
2023-02-07 21:31:44 +0700

    init

--- Assets/Sourse/Script/Camera.cs
+++ Assets/Sourse/Script/Camera.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
-    [SerializeField] private Transform _player;
+    [SerializeField] private Transform _target;
 
     private Vector3 _track;
     private float _dumping = 10;
 
     private void Update()
     {
-        _track = Vector3.MoveTowards(transform.position, _player.transform.position, _dumping * Time.deltaTime);
+        _track = Vector3.MoveTowards(transform.position, _target.transform.position, _dumping * Time.deltaTime);
         transform.position = new Vector3(_track.x, _track.y, transform.position.z);
     }
 }
